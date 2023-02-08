f1528372f61c08bd15f353894b1ccddbc73955ec
mccarthyk <mccarthyk@hcflgov.net>
2023-02-08 10:58:48 -0500

    add baseurl

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    baseURL: '/contentstack-customfield-event-generator/',
+  },
+
   css: [
     //
     '~~/assets/custom.scss',
